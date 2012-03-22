872ae67f05ea3cd61491a4e45b53382394435472
heat87 <heat87@arcor.de>
2012-03-22 03:09:57 +0100

    title modifier

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  #title modifier
+  def title
+    base_title = "Heat Page"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
